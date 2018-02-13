63c4a70bd00ca61bacceb252e48b119c2181c789
apfelstrudelweb <ulrich.vormbrock@gmail.com>
2018-02-13 11:27:54 +0100

    Refactoring uitcollectionview flow layout

--- Simple Back Trainer/HomeVC.swift
+++ Simple Back Trainer/HomeVC.swift
@@ -13,26 +13,44 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
     @IBOutlet weak var collectionView: UICollectionView!
     
     var cellWidth : CGFloat!
-    var gradientLayerArray = [CAGradientLayer]()
+    var flowLayout : UICollectionViewFlowLayout!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
-
-        let flow = collectionView.collectionViewLayout as! UICollectionViewFlowLayout // If you create collectionView programmatically then just create this flow by UICollectionViewFlowLayout() and init a collectionView by this flow.
-        
-        let itemSpacing: CGFloat = 40
-        let itemsInOneLine: CGFloat = 2
-        flow.sectionInset = UIEdgeInsetsMake(0, 40, 0, 40)
-        cellWidth = self.collectionView.bounds.width - itemSpacing * CGFloat(itemsInOneLine - 1) - flow.sectionInset.left - flow.sectionInset.right //
-        flow.itemSize = CGSize(width: floor(cellWidth/itemsInOneLine), height: 1.4*cellWidth/itemsInOneLine)
-        flow.minimumInteritemSpacing = 40
-        flow.minimumLineSpacing = 20
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.reloadData()
     }
     
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
     var items = [["title":"Starker Rücken für Berufe", "icon":"pictogramm"],["title":"Rückenmuskeln für Sportler", "icon":"pictogramm"],["title":"Trainings-Übungen", "icon":"pictogramm"],["title":"Mobilisierungs-Übungen", "icon":"pictogramm"],["title":"Trainingsplan", "icon":"pictogramm"],["title":"Rückenschule", "icon":"pictogramm"]]
     
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow = UIScreen.main.bounds.height > UIScreen.main.bounds.width ? 2 : 4
+
+        flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.minimumInteritemSpacing = 20
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 20, 0, 20)
+        let totalSpace = flowLayout.sectionInset.left
+            + flowLayout.sectionInset.right
+            + (flowLayout.minimumInteritemSpacing * CGFloat(numberOfItemsPerRow - 1))
+        var size = CGFloat((collectionView.bounds.width - totalSpace) / CGFloat(numberOfItemsPerRow))
+        
+        if size > 200.0 {
+            size = CGFloat(200)
+        }
+        
+        flowLayout.itemSize = CGSize(width: size, height: 1.4*size)
+        
+        return CGSize(width: size, height: 1.4*size)
+    }
     
     // MARK: - UICollectionViewDataSource protocol
     
@@ -46,6 +64,7 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
         
         // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ItemsCollectionViewCell
+        
         
         cell.label.text = items[indexPath.row]["title"]!
         cell.imageView.image = UIImage(named: items[indexPath.row]["icon"]!)
@@ -69,17 +88,25 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
             gradientColors = [UIColor.red.withAlphaComponent(1).cgColor, UIColor.red.withAlphaComponent(0.7).cgColor]
         }
         
+        // remove previous layer after device rotation
+        for layer in cell.gradientView.layer.sublayers! {
+            if layer.isKind(of: CAGradientLayer.self) {
+                layer.removeFromSuperlayer()
+            }
+        }
+        
         let gradientLayer:CAGradientLayer = CAGradientLayer()
         gradientLayer.colors = gradientColors
         cell.gradientView.layer.addSublayer(gradientLayer)
 
-        //gradientLayer.frame = CGRect(x: 0, y: 0, width: 0.8*cellWidth, height: 0.5*cellWidth)
         gradientLayer.zPosition = -1
-        gradientLayer.masksToBounds = true
         gradientLayer.cornerRadius = 20
         cell.gradientView.layer.cornerRadius = 20
-        gradientLayer.frame = CGRect(x: 0, y: 0, width: 0.5*cellWidth, height: 0.5*cellWidth)
-        //gradientLayerArray.append(gradientLayer)
+        
+        let cellSize = self.flowLayout.itemSize
+
+        
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: cellSize.width, height: cellSize.height/1.4)
 
         return cell
     }
@@ -91,13 +118,5 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
-    
-//    override func viewDidLayoutSubviews() {
-//        for layer in gradientLayerArray {
-//            layer.frame = CGRect(x: 0, y: 0, width: 0.5*cellWidth, height: 0.5*cellWidth)
-//
-//        }
-//    }
-
 
 }
