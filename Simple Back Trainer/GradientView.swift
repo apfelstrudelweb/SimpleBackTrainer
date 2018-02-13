d6fdaee217994772815d2c26bbdd36c8686ab955
apfelstrudelweb <ulrich.vormbrock@gmail.com>
2018-02-13 16:00:49 +0100

    Refactoring of icon collection view

---
+++ Simple Back Trainer/GradientView.swift
@@ -0,0 +1,29 @@
+//
+//  GradientView.swift
+//  Simple Back Trainer
+//
+//  Created by Ulrich Vormbrock on 13.02.18.
+//  Copyright © 2018 Kode. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    var gradientLayer: CAGradientLayer!
+    
+    override open class var layerClass: AnyClass {
+        return CAGradientLayer.classForCoder()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        gradientLayer = self.layer as! CAGradientLayer
+        gradientLayer.cornerRadius = 20
+        //gradientLayer.colors = [UIColor.yellow.cgColor, UIColor.red.cgColor]
+    }
+    
+    public func setColors(for colors: NSArray) {
+        gradientLayer.colors = colors as? [Any]
+    }
+}
--- Simple Back Trainer/HomeVC.swift
+++ Simple Back Trainer/HomeVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collectionView: UICollectionView!
@@ -22,10 +24,6 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
         addSlideMenuButton()
     }
     
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-        collectionView.reloadData()
-    }
     
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
     var items = [["title":"Starker Rücken für Berufe", "icon":"pictogramm"],["title":"Rückenmuskeln für Sportler", "icon":"pictogramm"],["title":"Trainings-Übungen", "icon":"pictogramm"],["title":"Mobilisierungs-Übungen", "icon":"pictogramm"],["title":"Trainingsplan", "icon":"pictogramm"],["title":"Rückenschule", "icon":"pictogramm"]]
@@ -33,31 +31,21 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let numberOfItemsPerRow = UIScreen.main.bounds.height > UIScreen.main.bounds.width ? 2 : 3
+        var numberOfItemsPerRow = UIScreen.main.bounds.height > UIScreen.main.bounds.width ? 2 : 3
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            numberOfItemsPerRow *= 2
+        }
 
         let margin = 0.1*UIScreen.main.bounds.width
         flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        flowLayout.minimumInteritemSpacing = margin
+        flowLayout.minimumInteritemSpacing = 5
         flowLayout.sectionInset = UIEdgeInsetsMake(0, margin, 0, margin)
         let totalSpace = flowLayout.sectionInset.left
             + flowLayout.sectionInset.right
             + (flowLayout.minimumInteritemSpacing * CGFloat(numberOfItemsPerRow - 1))
-        var size = CGFloat((collectionView.bounds.width - totalSpace) / CGFloat(numberOfItemsPerRow))
+        let size = CGFloat((collectionView.bounds.width - totalSpace) / CGFloat(numberOfItemsPerRow))
         
-        if size > 200.0 {
-            size = 200.0
-        }
-        
-        var fact : CGFloat = 1.4
-        
-        if UIScreen.main.bounds.height < 800.0 {
-            fact += (812.0 / UIScreen.main.bounds.height)
-            fact -= 1.0
-        }
-        
-        flowLayout.itemSize = CGSize(width: size, height: fact*size)
-        
-        return flowLayout.itemSize
+        return CGSize(width: size, height: 1.2*size)
     }
     
     // MARK: - UICollectionViewDataSource protocol
@@ -73,48 +61,29 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
         // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ItemsCollectionViewCell
         
-        let cellSize = self.flowLayout.itemSize
         cell.label.text = items[indexPath.row]["title"]!
-        
-        //cell.label.fitTextToBounds(for: CGRect(x: 0, y: 0, width: cellSize.width, height: cellSize.width / 1.8))
-
-
         cell.imageView.image = UIImage(named: items[indexPath.row]["icon"]!)
         
         var gradientColors: [CGColor]!
         
         switch indexPath.row {
-        case 0: gradientColors = [UIColor.green.withAlphaComponent(1).cgColor, UIColor.green.withAlphaComponent(0.5).cgColor]
+        case 0: gradientColors = [UIColor.green.withAlphaComponent(1).cgColor, UIColor.green.withAlphaComponent(0.4).cgColor]
             break
-        case 1: gradientColors = [UIColor.yellow.withAlphaComponent(1).cgColor, UIColor.yellow.withAlphaComponent(0.5).cgColor]
+        case 1: gradientColors = [UIColor.yellow.withAlphaComponent(1).cgColor, UIColor.yellow.withAlphaComponent(0.4).cgColor]
             break
-        case 2: gradientColors = [UIColor.red.withAlphaComponent(1).cgColor, UIColor.red.withAlphaComponent(0.5).cgColor]
+        case 2: gradientColors = [UIColor.red.withAlphaComponent(1).cgColor, UIColor.red.withAlphaComponent(0.4).cgColor]
             break
-        case 3: gradientColors = [UIColor.blue.withAlphaComponent(1).cgColor, UIColor.blue.withAlphaComponent(0.5).cgColor]
+        case 3: gradientColors = [UIColor.blue.withAlphaComponent(1).cgColor, UIColor.blue.withAlphaComponent(0.4).cgColor]
             break
-        case 4: gradientColors = [UIColor.orange.withAlphaComponent(1).cgColor, UIColor.orange.withAlphaComponent(0.5).cgColor]
+        case 4: gradientColors = [UIColor.orange.withAlphaComponent(1).cgColor, UIColor.orange.withAlphaComponent(0.4).cgColor]
             break
-        case 5: gradientColors = [UIColor.cyan.withAlphaComponent(1).cgColor, UIColor.cyan.withAlphaComponent(0.5).cgColor]
+        case 5: gradientColors = [UIColor.cyan.withAlphaComponent(1).cgColor, UIColor.cyan.withAlphaComponent(0.4).cgColor]
             break
         default:
-            gradientColors = [UIColor.red.withAlphaComponent(1).cgColor, UIColor.red.withAlphaComponent(0.7).cgColor]
+            gradientColors = [UIColor.red.withAlphaComponent(1).cgColor, UIColor.red.withAlphaComponent(0.4).cgColor]
         }
-        
-        // remove previous layer after device rotation
-        for layer in cell.gradientView.layer.sublayers! {
-            if layer.isKind(of: CAGradientLayer.self) {
-                layer.removeFromSuperlayer()
-            }
-        }
-        
-        let gradientLayer:CAGradientLayer = CAGradientLayer()
-        gradientLayer.colors = gradientColors
-        cell.gradientView.layer.addSublayer(gradientLayer)
 
-        gradientLayer.zPosition = -1
-        gradientLayer.cornerRadius = 20
-
-        gradientLayer.frame = CGRect(x: 0, y: 0, width: cellSize.width, height: cellSize.width)
+        (cell.gradientView as! GradientView).setColors(for: gradientColors! as NSArray)
 
         return cell
     }
@@ -127,60 +96,4 @@ class HomeVC: BaseViewController, UICollectionViewDataSource, UICollectionViewDe
         print("You selected cell #\(indexPath.item)!")
     }
 
-}
-
-extension UIFont {
-    
-    /**
-     Will return the best approximated font size which will fit in the bounds.
-     If no font with name `fontName` could be found, nil is returned.
-     */
-    static func bestFitFontSize(for text: String, in bounds: CGRect, fontName: String) -> CGFloat? {
-        var maxFontSize: CGFloat = 32.0 // UIKit best renders with factors of 2
-        guard let maxFont = UIFont(name: fontName, size: maxFontSize) else {
-            return nil
-        }
-        
-        let textWidth = text.width(withConstraintedHeight: bounds.height, font: maxFont)
-        let textHeight = text.height(withConstrainedWidth: bounds.width, font: maxFont)
-        
-        // Determine the font scaling factor that should allow the string to fit in the given rect
-        let scalingFactor = min(bounds.width / textWidth, bounds.height / textHeight)
-        
-        // Adjust font size
-        maxFontSize *= scalingFactor
-        
-        return floor(maxFontSize)
-    }
-}
-
-extension UILabel {
-    
-    /// Will auto resize the contained text to a font size which fits the frames bounds
-    /// Uses the pre-set font to dynamicly determine the proper sizing
-    func fitTextToBounds(for rect: CGRect) {
-        guard let text = text, let currentFont = font else { return }
-        
-        if let dynamicFontSize = UIFont.bestFitFontSize(for: text, in: rect, fontName: currentFont.fontName) {
-            font = UIFont(name: currentFont.fontName, size: dynamicFontSize)
-        }
-    }
-    
-}
-
-fileprivate extension String {
-    
-    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
-        
-        return ceil(boundingBox.height)
-    }
-    
-    func width(withConstraintedHeight height: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
-        
-        return ceil(boundingBox.width)
-    }
 }
