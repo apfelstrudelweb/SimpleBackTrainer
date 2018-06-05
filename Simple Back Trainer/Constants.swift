a8b2797c2e1ff1b056673eff74abb886072c44f7
apfelstrudelweb <ulrich.vormbrock@gmail.com>
2018-06-05 19:04:35 +0200

    first commit

---
+++ Simple Back Trainer/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Simple Back Trainer
+//
+//  Created by Rakesh Kumar on 03/06/18.
+//  Copyright © 2018 Kode. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
