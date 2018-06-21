58317bc067758ff7c45272385c683ffaeaea1017
apfelstrudelweb <ulrich.vormbrock@gmail.com>
2018-06-21 22:08:34 +0200

    filtering of Trainingsübungen - first step

--- Simple Back Trainer/ExercisesTableViewController.swift
+++ Simple Back Trainer/ExercisesTableViewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import AMPopTip
+import CoreData
 
-class ExercisesTableViewController: UITableViewController {
+class ExercisesTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     
     let inactiveColor:UIColor = .lightGray
+    var popTip = PopTip()
+    
+    var fetchedResultsController: NSFetchedResultsController<Workout>!
+    var fetchRequest: NSFetchRequest<Workout>!
 
 
     @IBOutlet weak var buttonHandweight: UIButton!
@@ -37,17 +43,29 @@ class ExercisesTableViewController: UITableViewController {
         buttonBall.setTitle("", for: .normal)
         buttonMachine.setTitle("", for: .normal)
         
-//        buttonHandweight.setTitleColor(buttonHandweight.tintColor, for: .normal)
-//        buttonBand.setTitleColor(buttonBand.tintColor, for: .normal)
-//        buttonMat.setTitleColor(buttonMat.tintColor, for: .normal)
-//        buttonBall.setTitleColor(buttonBall.tintColor, for: .normal)
-//        buttonMachine.setTitleColor(buttonMachine.tintColor, for: .normal)
-//
-//        buttonHandweight.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: 5.0, bottom: 0.0, right: 0.0)
-//        buttonBand.titleEdgeInsets = buttonHandweight.titleEdgeInsets
-//        buttonMat.titleEdgeInsets = buttonHandweight.titleEdgeInsets
-//        buttonBall.titleEdgeInsets = buttonHandweight.titleEdgeInsets
-//        buttonMachine.titleEdgeInsets = buttonHandweight.titleEdgeInsets
+        popTip.font = UIFont(name: "Avenir-Medium", size: UI_USER_INTERFACE_IDIOM() == .pad ? 20 : 14)!
+        popTip.shouldDismissOnTap = true
+        popTip.shouldDismissOnTapOutside = true
+        popTip.shouldDismissOnSwipeOutside = true
+        popTip.edgeMargin = 5
+        popTip.offset = 2
+        popTip.bubbleOffset = 0
+        popTip.edgeInsets = UIEdgeInsetsMake(0, 10, 0, 10)
+        
+        self.tableView.register(UINib(nibName: "VideoCell", bundle: nil), forCellReuseIdentifier: "videoCell")
+        
+        self.fetchRequest = NSFetchRequest<Workout> (entityName: "Workout")
+        self.fetchRequest.sortDescriptors = [NSSortDescriptor (key: "position", ascending: true)]
+        
+        self.fetchedResultsController = NSFetchedResultsController<Workout> (
+            fetchRequest: fetchRequest,
+            managedObjectContext: CoreDataManager.sharedInstance.managedObjectContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        self.fetchedResultsController.delegate = self
+        
+        self.filterHandweight((Any).self)
+
     }
     
     func clearButtons() {
@@ -65,97 +83,150 @@ class ExercisesTableViewController: UITableViewController {
         clearButtons()
         buttonHandweight.tintColor = self.navigationController?.navigationBar.barTintColor
         
+        let predicate1 = NSPredicate(format: "musclegroupId.id < %d", 5)
+        let predicate2 = NSPredicate(format: "id = %d", 3)
+        let compound:NSCompoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        self.fetchRequest.predicate = compound
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
     }
     
     @IBAction func filterBand(_ sender: Any) {
         
         clearButtons()
         buttonBand.tintColor = self.navigationController?.navigationBar.barTintColor
+        
+        let predicate1 = NSPredicate(format: "musclegroupId.id < %d", 5)
+        let predicate2 = NSPredicate(format: "id = %d", 2)
+        let compound:NSCompoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        self.fetchRequest.predicate = compound
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
     @IBAction func filterMat(_ sender: Any) {
         
         clearButtons()
         buttonMat.tintColor = self.navigationController?.navigationBar.barTintColor
+        
+        let predicate1 = NSPredicate(format: "musclegroupId.id < %d", 5)
+        let predicate2 = NSPredicate(format: "id = %d", 1)
+        let compound:NSCompoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        self.fetchRequest.predicate = compound
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
     @IBAction func filterBall(_ sender: Any) {
         
         clearButtons()
         buttonBall.tintColor = self.navigationController?.navigationBar.barTintColor
+        
+        let predicate1 = NSPredicate(format: "musclegroupId.id >= %d", 5)
+        let predicate2 = NSPredicate(format: "id = %d", 3)
+        let compound:NSCompoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        self.fetchRequest.predicate = compound
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
     @IBAction func filterMachine(_ sender: Any) {
         
         clearButtons()
         buttonMachine.tintColor = self.navigationController?.navigationBar.barTintColor
+        
+        let predicate1 = NSPredicate(format: "musclegroupId.id >= %d", 5)
+        let predicate2 = NSPredicate(format: "id = %d", 2)
+        let compound:NSCompoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        self.fetchRequest.predicate = compound
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        guard fetchedResultsController != nil else {
+            return 0
+        }
+        if let sections = fetchedResultsController.sections {
+            let currentSection = sections[section]
+            return currentSection.numberOfObjects
+        }
+        
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:"videoCell", for: indexPath) as! VideoCell
+        
+        let workout = fetchedResultsController.object(at: indexPath)
+        
+        cell.buttonColor = (navigationController?.navigationBar.barTintColor)!
+        cell.videoLabel?.text = workout.name
+        
+        cell.videoImageView?.image = UIImage(data:workout.icon! as Data, scale:1.0)
+        cell.indexPath = indexPath
+        cell.delegate = self
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+}
+
+extension ExercisesTableViewController: VideoCellDelegate {
+    
+    func infoButtonTouched(_ sender: UIButton, indexPath: IndexPath, x: Int) {
+        
+        DispatchQueue.main.async {
+            let rect = self.tableView.rectForRow(at: indexPath)
+            
+            let frame = sender.frame.offsetBy(dx: 0, dy: rect.origin.y)
+            let tooltipWidth = frame.origin.x - CGFloat(x)
+            
+            self.popTip.bubbleColor = sender.backgroundColor!
+            
+            let workout = self.fetchedResultsController.object(at: indexPath)
+            
+            self.popTip.show(text: workout.descr!, direction: .left, maxWidth: tooltipWidth, in: self.view, from: frame, duration: 6)
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
